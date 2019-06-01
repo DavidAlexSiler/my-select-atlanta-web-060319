95cc8a104ca552cac430838cadf5595d3e4781fa
Alex Siler <davidalexsiler@gmail.com>
2019-06-01 04:19:56 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,19 @@
 def my_select(collection)
- # your code here!
+if collection.count == 0
+  return nil
+else
+  counter = 0
+  new_arr = []
+  while counter < collection.count
+    if collection[counter] % 2 == 0
+				yield collection[counter]
+				new_arr << collection[counter]
+				counter += 1
+			else
+				puts collection[counter]
+				counter += 1
+			end
+		end
+		return new_arr
+ 	end
 end

40eb1e6102c9fe2c6e720c12facf7206398882a2
Alex Siler <davidalexsiler@gmail.com>
2019-06-01 04:18:58 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -5,4 +5,15 @@ else
   counter = 0
   new_arr = []
   while counter < collection.count
+    if collection[counter] % 2 == 0
+				yield collection[counter]
+				new_arr << collection[counter]
+				counter += 1
+			else
+				puts collection[counter]
+				counter += 1
+			end
+		end
+		return new_arr
+ 	end
 end
